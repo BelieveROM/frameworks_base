a9776c79772bed27ccbc82234adbdc857db70125
tchaari <tarak.chaari@gmail.com>
2013-01-04 00:54:50 +0100

    Frameworks: Manage multiple QS tiles instances order
    Patchset 4:
    - moved hard coded string to strings.xml
    
    Change-Id: I629be00c5fe71ec8e6a7bb8335c066f86dec4430
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTypeTile.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/ProfileTile.java

--- packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTypeTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTypeTile.java
@@ -16,141 +16,207 @@
 
 package com.android.systemui.quicksettings;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
 import android.os.Handler;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.view.View.OnLongClickListener;
+import android.view.View.OnClickListener;
 
+import com.android.internal.telephony.Phone;
 import com.android.systemui.R;
-import com.android.systemui.statusbar.phone.QuickSettingsController;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
-import com.android.systemui.statusbar.policy.NetworkController;
-import com.android.systemui.statusbar.policy.NetworkController.NetworkSignalChangedCallback;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
 
-public class MobileNetworkTile extends QuickSettingsTile implements NetworkSignalChangedCallback{
+public class MobileNetworkTypeTile extends QuickSettingsTile {
 
-    private int mDataTypeIconId;
-    private String dataContentDescription;
-    private String signalContentDescription;
-    private boolean wifiOn = false;
+    private static final String TAG = "NetworkModeQuickSettings";
+
+    // retrieved from Phone.apk
+    private static final String ACTION_NETWORK_MODE_CHANGED = "com.android.internal.telephony.NETWORK_MODE_CHANGED";
+    private static final String ACTION_MODIFY_NETWORK_MODE = "com.android.internal.telephony.MODIFY_NETWORK_MODE";
+    private static final String EXTRA_NETWORK_MODE = "networkMode";
+
+    private static final int STATE_2G_AND_3G = 1;
+    private static final int STATE_2G_ONLY = 2;
+    private static final int STATE_TURNING_ON = 3;
+    private static final int STATE_TURNING_OFF = 4;
+    private static final int STATE_INTERMEDIATE = 5;
+    private static final int STATE_UNEXPECTED = 6;
+    private static final int NO_NETWORK_MODE_YET = -99;
+    private static final int NETWORK_MODE_UNKNOWN = -100;
+
+    private static final int CM_MODE_3G2G = 0;
+    private static final int CM_MODE_3GONLY = 1;
+    private static final int CM_MODE_BOTH = 2;
+
+    private int mMode = NO_NETWORK_MODE_YET;
+    private int mIntendedMode = NO_NETWORK_MODE_YET;
+    private int mInternalState = STATE_INTERMEDIATE;
+    private int mState;
     public static QuickSettingsTile mInstance;
 
     public static QuickSettingsTile getInstance(Context context, LayoutInflater inflater,
-            QuickSettingsContainerView container, final QuickSettingsController qsc, Handler handler) {
-        if (mInstance == null) mInstance = new MobileNetworkTile(context, inflater, container, qsc);
+            QuickSettingsContainerView container, final QuickSettingsController qsc, Handler handler, String id) {
+        if (mInstance == null) mInstance = new MobileNetworkTypeTile(context, inflater, container, qsc);
         return mInstance;
     }
 
-    public MobileNetworkTile(Context context, LayoutInflater inflater,
-            QuickSettingsContainerView container, QuickSettingsController qsc) {
+    public MobileNetworkTypeTile(Context context,
+            LayoutInflater inflater, QuickSettingsContainerView container,
+            QuickSettingsController qsc) {
         super(context, inflater, container, qsc);
-        mTileLayout = R.layout.quick_settings_tile_rssi;
 
-        mOnClick = new View.OnClickListener() {
+        updateState();
 
+        mOnClick = new OnClickListener() {
             @Override
             public void onClick(View v) {
+                int currentMode = getCurrentCMMode();
+
+                Intent intent = new Intent(ACTION_MODIFY_NETWORK_MODE);
+                switch (mMode) {
+                    case Phone.NT_MODE_WCDMA_PREF:
+                    case Phone.NT_MODE_GSM_UMTS:
+                        intent.putExtra(EXTRA_NETWORK_MODE, Phone.NT_MODE_GSM_ONLY);
+                        mInternalState = STATE_TURNING_OFF;
+                        mIntendedMode = Phone.NT_MODE_GSM_ONLY;
+                        break;
+                    case Phone.NT_MODE_WCDMA_ONLY:
+                        if (currentMode == CM_MODE_3GONLY) {
+                            intent.putExtra(EXTRA_NETWORK_MODE, Phone.NT_MODE_GSM_ONLY);
+                            mInternalState = STATE_TURNING_OFF;
+                            mIntendedMode = Phone.NT_MODE_GSM_ONLY;
+                        } else {
+                            intent.putExtra(EXTRA_NETWORK_MODE, Phone.NT_MODE_WCDMA_PREF);
+                            mInternalState = STATE_TURNING_ON;
+                            mIntendedMode = Phone.NT_MODE_WCDMA_PREF;
+                        }
+                        break;
+                    case Phone.NT_MODE_GSM_ONLY:
+                        if (currentMode == CM_MODE_3GONLY || currentMode == CM_MODE_BOTH) {
+                            intent.putExtra(EXTRA_NETWORK_MODE, Phone.NT_MODE_WCDMA_ONLY);
+                            mInternalState = STATE_TURNING_ON;
+                            mIntendedMode = Phone.NT_MODE_WCDMA_ONLY;
+                        } else {
+                            intent.putExtra(EXTRA_NETWORK_MODE, Phone.NT_MODE_WCDMA_PREF);
+                            mInternalState = STATE_TURNING_ON;
+                            mIntendedMode = Phone.NT_MODE_WCDMA_PREF;
+                        }
+                        break;
+                }
+
+                mMode = NETWORK_MODE_UNKNOWN;
+                mContext.sendBroadcast(intent);
             }
         };
-        mOnLongClick = new OnLongClickListener() {
 
+        mOnLongClick = new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                Intent intent = new Intent();
-                intent.setComponent(new ComponentName(
-                        "com.android.settings",
-                        "com.android.settings.Settings$DataUsageSummaryActivity"));
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setClassName("com.android.phone", "com.android.phone.Settings");
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startSettingsActivity(intent);
                 return true;
             }
         };
+
+        qsc.registerAction(ACTION_NETWORK_MODE_CHANGED, this);
     }
 
     @Override
-    void onPostCreate() {
-        NetworkController controller = new NetworkController(mContext);
-        controller.addNetworkSignalChangedCallback(this);
-        super.onPostCreate();
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getExtras() != null) {
+            mMode = intent.getExtras().getInt(EXTRA_NETWORK_MODE);
+            //Update to actual state
+            mIntendedMode = mMode;
+        }
+
+        //need to clear intermediate states and update the tile
+        mInternalState = networkModeToState();
+        applyNetworkTypeChanges();
     }
 
-    @Override
-    public void onWifiSignalChanged(boolean enabled, int wifiSignalIconId,
-            String wifitSignalContentDescriptionId, String description) {
-        wifiOn = enabled;
-
+    private void applyNetworkTypeChanges(){
+        updateState();
+        updateQuickSettings();
     }
 
-    @Override
-    public void onMobileDataSignalChanged(boolean enabled,
-            int mobileSignalIconId, String mobileSignalContentDescriptionId,
-            int dataTypeIconId, String dataTypeContentDescriptionId,
-            String description) {
-        if (deviceSupportsTelephony()) {
-            // TODO: If view is in awaiting state, disable
-            Resources r = mContext.getResources();
-            mDrawable = enabled && (mobileSignalIconId > 0)
-                    ? mobileSignalIconId
-                    : R.drawable.ic_qs_signal_no_signal;
-            signalContentDescription = enabled && (mobileSignalIconId > 0)
-                    ? signalContentDescription
-                    : r.getString(R.string.accessibility_no_signal);
-            mDataTypeIconId = enabled && (dataTypeIconId > 0) && !wifiOn
-                    ? dataTypeIconId
-                    : 0;
-            dataContentDescription = enabled && (dataTypeIconId > 0) && !wifiOn
-                    ? dataContentDescription
-                    : r.getString(R.string.accessibility_no_data);
-            mLabel = enabled
-                    ? removeTrailingPeriod(description)
-                    : r.getString(R.string.quick_settings_rssi_emergency_only);
-            updateQuickSettings();
+    protected void updateState() {
+        mMode = get2G3G(mContext);
+        mState = networkModeToState();
+
+        mLabel = mContext.getString(R.string.quick_settings_network_type);
+
+        switch (mState) {
+            case STATE_UNEXPECTED:
+                mDrawable = R.drawable.ic_qs_unexpected_network;
+                break;
+            case STATE_2G_ONLY:
+                mDrawable = R.drawable.ic_qs_2g_on;
+                break;
+            case STATE_2G_AND_3G:
+                if (mMode == Phone.NT_MODE_WCDMA_ONLY) {
+                    mDrawable = R.drawable.ic_qs_3g_on;
+                } else {
+                    mDrawable = R.drawable.ic_qs_2g3g_on;
+                }
+                break;
+            case STATE_INTERMEDIATE:
+                if (mInternalState == STATE_TURNING_ON) {
+                    if (mIntendedMode == Phone.NT_MODE_WCDMA_ONLY) {
+                        mDrawable = R.drawable.ic_qs_3g_on;
+                    } else {
+                        mDrawable = R.drawable.ic_qs_2g3g_on;
+                    }
+                } else {
+                    mDrawable = R.drawable.ic_qs_2g_on;
+                }
+                break;
         }
     }
 
-    @Override
-    public void onAirplaneModeChanged(boolean enabled) {
-        // TODO Auto-generated method stub
-
-    }
-
-    boolean deviceSupportsTelephony() {
-        PackageManager pm = mContext.getPackageManager();
-        return pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
-    }
-
-    @Override
-    void updateQuickSettings() {
-        TextView tv = (TextView) mTile.findViewById(R.id.rssi_textview);
-        ImageView iv = (ImageView) mTile.findViewById(R.id.rssi_image);
-        ImageView iov = (ImageView) mTile.findViewById(R.id.rssi_overlay_image);
-        iv.setImageResource(mDrawable);
-        if (mDataTypeIconId > 0) {
-            iov.setImageResource(mDataTypeIconId);
-        } else {
-            iov.setImageDrawable(null);
+    private static int get2G3G(Context context) {
+        int state = 99;
+        try {
+            state = Settings.Global.getInt(context.getContentResolver(),
+                    Settings.Global.PREFERRED_NETWORK_MODE);
+        } catch (SettingNotFoundException e) {
+            // Do nothing
         }
-        tv.setText(mLabel);
-        mTile.setContentDescription(mContext.getResources().getString(
-                R.string.accessibility_quick_settings_mobile,
-                signalContentDescription, dataContentDescription,
-                mLabel));
+        return state;
     }
 
- // Remove the period from the network name
-    public static String removeTrailingPeriod(String string) {
-        if (string == null) return null;
-        final int length = string.length();
-        if (string.endsWith(".")) {
-            string.substring(0, length - 1);
+    private int networkModeToState() {
+        if (mInternalState == STATE_TURNING_ON || mInternalState == STATE_TURNING_OFF) {
+            return STATE_INTERMEDIATE;
         }
-        return string;
+
+        switch (mMode) {
+            case Phone.NT_MODE_WCDMA_PREF:
+            case Phone.NT_MODE_WCDMA_ONLY:
+            case Phone.NT_MODE_GSM_UMTS:
+                return STATE_2G_AND_3G;
+            case Phone.NT_MODE_GSM_ONLY:
+                return STATE_2G_ONLY;
+            case Phone.NT_MODE_CDMA:
+            case Phone.NT_MODE_CDMA_NO_EVDO:
+            case Phone.NT_MODE_EVDO_NO_CDMA:
+            case Phone.NT_MODE_GLOBAL:
+                // need to check what is going on
+                Log.d(TAG, "Unexpected network mode (" + mMode + ")");
+                return STATE_UNEXPECTED;
+        }
+        return STATE_INTERMEDIATE;
     }
 
+    private int getCurrentCMMode() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.EXPANDED_NETWORK_MODE,
+                CM_MODE_3G2G);
+    }
 }
