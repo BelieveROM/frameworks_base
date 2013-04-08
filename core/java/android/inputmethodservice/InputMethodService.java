59c7be1d9bc14b506717f0599f7556a86cd094d4
Lars Greiss <kufikugel@googlemail.com>
2013-04-08 06:39:38 +0200

    Frameworks: SlimPIE extended (1/2)
    This commit introduces SlimPIE. A very popular feature which
    was till now for us no point to integrate it because it did
    not fit our concept. Now I found a bit time to program a PIE
    which is much more extended then the usual kanged PIE on
    most roms but still resource friendly. At this point
    many thanks for the great basic idea to PA and the work
    from Jens Doll from CM. Based on this ideas SlimPIE goes
    it's way now.
    
    @kangers....we would like to see if you can wait till we
    released this feature this week.  Thanks a lot.
    
    Features of SlimPIE
    
    - custom Buttons (user can choose between 11 different default actions (like navbar/hardware key bind)
      + custom app/shortcut for every button
    - custom longpress action for every button (user has  same possibility like the normal custom Button)
    - button quantitiy up to 5 buttons (so 5 + 5 longpress actions are possible)
    - selectable trigger positions + trigger width (with optical help to set up)
    - turn off text and status information + abillity to hide statusbar information which are redundant
    - turn off snap points
    - turn off background
    - ability to use SlimPIE only on expanded desktop as well as using it always
    - integrated logic disable triggers which can conflict with system actions
      (like statusbar or navbar.....SlimPIE detects if one is present and turns this edge off
      temporaly)
    - same for snap points
    - option to disable bottom trigger and reduce left and right trigger when
      IME keyboard is active
    
    Style feature:
    
    - button color + transparency
    - button pressed and button longpress color + transparency
    - button outline color
    - snap point color
    - text color
    - background color + transparency
    - for every shortcut user can choose a  custom icon from gallery
    - custom icon color
    - custom icon  color mode (only system icons eg or gallery icons + system icons or all icons)
    - Pie size
    
    Also it is possible via overlay to set a minimum, default and max trigger height
    due that some phones need a bit adjustment on that (this setting reflects on
    the trigger seekbar in settings user can adjust)
    
    Screenshots:
    
    https://dl.dropboxusercontent.com/u/106208842/use_it_for_app_shortcuts.png
    https://dl.dropboxusercontent.com/u/106208842/other_colors_or_icons_no_problem.png
    
    Last PatchSet:
    - reduce trigger sizes and disable bottom trigger if IME shows up
      optional in trigger settings, default value is on
    - DEBUG mod fixes
    - autoHide feature worked in
    - string corrections
    - some minor fixes
    
    Change-Id: I9bcc88375bbd770b0f5bd24fc84aaf7cdbf17238
    
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/CircleBattery.java

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -28,9 +28,13 @@ import android.content.res.TypedArray;
 import android.graphics.Rect;
 import android.graphics.Region;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.ResultReceiver;
 import android.os.SystemClock;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.provider.Settings;
 import android.text.InputType;
 import android.text.Layout;
@@ -249,6 +253,20 @@ public class InputMethodService extends AbstractInputMethodService {
      */
     public static final int IME_VISIBLE = 0x2;
 
+    int mVolumeKeyCursorControl = 0;
+    /**
+     * @hide
+     */
+    public static final int VOLUME_CURSOR_OFF = 0;
+    /**
+     * @hide
+     */
+    public static final int VOLUME_CURSOR_ON = 1;
+    /**
+     * @hide
+     */
+    public static final int VOLUME_CURSOR_ON_REVERSE = 2;
+
     InputMethodManager mImm;
     
     int mTheme = 0;
@@ -302,6 +320,11 @@ public class InputMethodService extends AbstractInputMethodService {
     
     int mStatusIcon;
     int mBackDisposition;
+
+    boolean mForcedAutoRotate;
+    Handler mHandler;
+
+    boolean mPermissionAllowsFeature;
 
     final Insets mTmpInsets = new Insets();
     final int[] mTmpLocation = new int[2];
@@ -658,6 +681,36 @@ public class InputMethodService extends AbstractInputMethodService {
         if (mHardwareAccelerated) {
             mWindow.getWindow().addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
         }
+
+        try {
+            mPermissionAllowsFeature = false;
+            String[] currentDefaultImePackage = null;
+            PackageManager pm = getPackageManager();
+
+            String defaultImePackage = Settings.Secure.getString(
+                getContentResolver(), Settings.Secure.DEFAULT_INPUT_METHOD);
+            if (defaultImePackage != null) {
+                currentDefaultImePackage = defaultImePackage.split("/", 2);
+            }
+            PackageInfo packageInfo = pm.getPackageInfo(currentDefaultImePackage[0], PackageManager.GET_PERMISSIONS);
+            String[] requestedPermissions = packageInfo.requestedPermissions;
+            if(requestedPermissions != null) {
+                for (int i = 0; i < requestedPermissions.length; i++) {
+                    if (requestedPermissions[i].equals(android.Manifest.permission.WRITE_SETTINGS)) {
+                        mPermissionAllowsFeature = true;
+                    }
+                }
+            }
+        } catch (NameNotFoundException e) {
+        }
+
+        if (mPermissionAllowsFeature) {
+            //IME is not showing on first onCreate to be sure
+            //toggle it off for PIE
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_SOFTKEYBOARD_IS_SHOWING, 0);
+        }
+
         initViews();
         mWindow.getWindow().setLayout(MATCH_PARENT, WRAP_CONTENT);
     }
@@ -713,6 +766,8 @@ public class InputMethodService extends AbstractInputMethodService {
         mCandidatesVisibility = getCandidatesHiddenVisibility();
         mCandidatesFrame.setVisibility(mCandidatesVisibility);
         mInputFrame.setVisibility(View.GONE);
+
+        mHandler = new Handler();
     }
 
     @Override public void onDestroy() {
@@ -878,7 +933,14 @@ public class InputMethodService extends AbstractInputMethodService {
      * is currently running in fullscreen mode.
      */
     public void updateFullscreenMode() {
-        boolean isFullscreen = mShowInputRequested && onEvaluateFullscreenMode();
+        boolean fullScreenOverride = Settings.System.getInt(getContentResolver(),
+                Settings.System.DISABLE_FULLSCREEN_KEYBOARD, 0) != 0;
+        boolean isFullscreen;
+        if (fullScreenOverride) {
+            isFullscreen = false;
+        } else {
+            isFullscreen = mShowInputRequested && onEvaluateFullscreenMode();
+        }
         boolean changed = mLastShowInputRequested != mShowInputRequested;
         if (mIsFullscreen != isFullscreen || !mFullscreenApplied) {
             changed = true;
@@ -1415,6 +1477,27 @@ public class InputMethodService extends AbstractInputMethodService {
             mWindowWasVisible = true;
             mInShowWindow = false;
         }
+
+        if (mPermissionAllowsFeature) {
+            //IME softkeyboard is showing....toggle it
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_SOFTKEYBOARD_IS_SHOWING, 1);
+
+            int mKeyboardRotationTimeout = Settings.System.getInt(getContentResolver(),
+                    Settings.System.KEYBOARD_ROTATION_TIMEOUT, 0);
+            if (mKeyboardRotationTimeout > 0) {
+                mHandler.removeCallbacks(restoreAutoRotation);
+                if (!mForcedAutoRotate) {
+                    boolean isAutoRotate = (Settings.System.getInt(getContentResolver(),
+                            Settings.System.ACCELEROMETER_ROTATION, 0) == 1);
+                    if (!isAutoRotate) {
+                        mForcedAutoRotate = true;
+                        Settings.System.putInt(getContentResolver(),
+                                Settings.System.ACCELEROMETER_ROTATION, 1);
+                    }
+                }
+            }
+        }
     }
 
     void showWindowInner(boolean showInput) {
@@ -1497,7 +1580,31 @@ public class InputMethodService extends AbstractInputMethodService {
             onWindowHidden();
             mWindowWasVisible = false;
         }
+        if (mPermissionAllowsFeature) {
+            //IME softkeyboard is hiding....toggle it
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_SOFTKEYBOARD_IS_SHOWING, 0);
+
+            int mKeyboardRotationTimeout = Settings.System.getInt(getContentResolver(),
+                    Settings.System.KEYBOARD_ROTATION_TIMEOUT, 0);
+            if (mKeyboardRotationTimeout > 0) {
+                mHandler.removeCallbacks(restoreAutoRotation);
+                if (mForcedAutoRotate) {
+                    mHandler.postDelayed(restoreAutoRotation, mKeyboardRotationTimeout);
+                }
+            }
+        }
     }
+ 
+    final Runnable restoreAutoRotation = new Runnable() {
+        @Override public void run() {
+            if (mPermissionAllowsFeature) {
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.ACCELEROMETER_ROTATION, 0);
+                mForcedAutoRotate = false;
+            }
+        }
+    };
 
     /**
      * Called when the input method window has been shown to the user, after
@@ -1767,6 +1874,26 @@ public class InputMethodService extends AbstractInputMethodService {
             }
             return false;
         }
+        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) {
+            mVolumeKeyCursorControl = Settings.System.getInt(getContentResolver(),
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
+            if (isInputViewShown() && (mVolumeKeyCursorControl != VOLUME_CURSOR_OFF)) {
+                sendDownUpKeyEvents((mVolumeKeyCursorControl == VOLUME_CURSOR_ON_REVERSE)
+                        ? KeyEvent.KEYCODE_DPAD_RIGHT : KeyEvent.KEYCODE_DPAD_LEFT);
+                return true;
+            }
+            return false;
+        }
+        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            mVolumeKeyCursorControl = Settings.System.getInt(getContentResolver(),
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
+            if (isInputViewShown() && (mVolumeKeyCursorControl != VOLUME_CURSOR_OFF)) {
+                sendDownUpKeyEvents((mVolumeKeyCursorControl == VOLUME_CURSOR_ON_REVERSE)
+                        ? KeyEvent.KEYCODE_DPAD_LEFT : KeyEvent.KEYCODE_DPAD_RIGHT);
+                return true;
+            }
+            return false;
+        }
         return doMovementKey(keyCode, event, MOVEMENT_DOWN);
     }
 
@@ -1812,7 +1939,15 @@ public class InputMethodService extends AbstractInputMethodService {
                 && !event.isCanceled()) {
             return handleBack(true);
         }
-        
+        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP
+                 || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            mVolumeKeyCursorControl = Settings.System.getInt(getContentResolver(),
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
+            if (isInputViewShown() && (mVolumeKeyCursorControl != VOLUME_CURSOR_OFF)) {
+                return true;
+            }
+            return false;
+        }
         return doMovementKey(keyCode, event, MOVEMENT_UP);
     }
 
