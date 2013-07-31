75940f001fb747a8010ae5c77e492878cc0fb4df
Jesus David <jesdga95@gmail.com>
2013-07-30 22:54:17 -0500

    Fix recent apps clear all
    Change-Id: I776e56f3a982592ba22acbd5c983916c508aa263

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1111,8 +1111,12 @@ public class PhoneStatusBar extends BaseStatusBar {
 
     private View.OnClickListener mRecentsClickListener = new View.OnClickListener() {
         public void onClick(View v) {
-            awakenDreams();
-            toggleRecentApps();
+            if(isRecentAppsVisible() && hasRecentApps()) {
+                clearRecentApps();
+            } else {
+                awakenDreams();
+                toggleRecentApps();
+            }
         }
     };
 
