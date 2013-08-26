adbc178681836b217f2a5644c220f25a5153fc46
torrented <wallace.ryan10@gmail.com>
2013-08-26 12:08:47 -0300

    ListView Animations (1/2)
    Change-Id: I5e72934c148daf3d3b36a5812f51d29c7fd98c94
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2773,6 +2773,7 @@ public final class Settings {
 	/**
          * Relative pie size (fraction)
          * Default: 1.0f
+         * @hide
          */
         public static final String PIE_SIZE = "pie_size";
 
@@ -3035,8 +3036,35 @@ public final class Settings {
          * @hide
          */
         public static final String QS_DYNAMIC_WIFI = "qs_dyanmic_wifi";
+        
+	/**
+         * ListView Animations
+         * 0 == None
+         * 1 == Wave (Left)
+         * 2 == Wave (Right)
+         * 3 == Scale
+         * 4 == Alpha
+         * 5 == Stack (Top)
+         * 6 == Stack (Bottom)
+         * 7 == Translate (Left)
+         * 8 == Translate (Right)
+         */
+        public static final String LISTVIEW_ANIMATION = "listview_animation";
 
-         /**
+        /**
+         * ListView Interpolators
+         * 0 == None
+         * 1 == accelerate_interpolator
+         * 2 == decelerate_interpolator
+         * 3 == accelerate_decelerate_interpolator
+         * 4 == anticipate_interpolator
+         * 5 == overshoot_interpolator
+         * 6 == anticipate_overshoot_interpolator
+         * 7 == bounce_interpolator
+         */
+        public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+ 	 /**
          * Quick Settings Quick Pulldown
          *
          * @hide
